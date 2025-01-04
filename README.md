fc20c837ba1fd9c49f64b09a68b8c13629d08023
Andrey Semenov <i@itmage.ru>
2025-01-04 22:01:14 +0300

    Added example for AES

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
     gpg --gen-key
     gpg --export --armor baza > public_key.asc
+
+## Links
+
+https://habr.com/ru/companies/otus/articles/833714/
---
+++ src/cli/examples/AES-256-GCM.rs
@@ -0,0 +1,30 @@
+use aes_gcm::aead::{Aead, KeyInit};
+use aes_gcm::{Aes256Gcm, Nonce};
+use rand::Rng;
+
+fn encrypt_data(plaintext: &[u8], key: &[u8]) -> Vec<u8> {
+    let cipher = Aes256Gcm::new(key.into());
+    let binding = rand::thread_rng().gen::<[u8; 12]>();
+    let nonce = Nonce::from_slice(&binding);
+    let ciphertext = cipher
+        .encrypt(nonce, plaintext)
+        .expect("Ошибка шифрования!");
+    [nonce.as_slice(), &ciphertext].concat()
+}
+
+fn decrypt_data(ciphertext: &[u8], key: &[u8]) -> Vec<u8> {
+    let cipher = Aes256Gcm::new(key.into());
+    let nonce = Nonce::from_slice(&ciphertext[..12]);
+    let ciphertext = &ciphertext[12..];
+    cipher.decrypt(nonce, ciphertext)
+        .expect("Ошибка дешифрования!")
+}
+
+fn main() {
+    let plaintext = b"This is a secret message!";
+    let key = rand::thread_rng().gen::<[u8; 32]>();
+    let ciphertext = encrypt_data(plaintext, &key);
+    let plaintext = decrypt_data(&ciphertext, &key);
+    println!("Зашифрованные данные: {:?}", ciphertext);
+    println!("Расшифрованные данные: {:?}", String::from_utf8_lossy(&plaintext));
+}
